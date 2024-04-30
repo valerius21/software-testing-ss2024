62810521050134e2131c39c37c33f61f7ad5eda2
Valerius Mattfeld <mail@valerius.me>
2024-04-30 14:55:42 +0200

    add tests for Misc.java

---
+++ src/test/java/com/softwaretesting/testing/util/MiscTest.java
@@ -0,0 +1,38 @@
+package com.softwaretesting.testing.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MiscTest {
+
+    @Test
+    void sum() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Test
+    void divide() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Test
+    void isColorSupported() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Test
+    void calculateFactorial() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Test
+    void isPrime() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Test
+    void isEven() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

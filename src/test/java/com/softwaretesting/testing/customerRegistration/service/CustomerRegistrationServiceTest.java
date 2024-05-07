a00cc4b8bc5b5521a63a772daa24398eff6e08a0
Valerius Mattfeld <mail@valerius.me>
2024-05-08 01:52:41 +0200

    add test file CustomerRegistrationServiceTest.java

---
+++ src/test/java/com/softwaretesting/testing/customerRegistration/service/CustomerRegistrationServiceTest.java
@@ -0,0 +1,13 @@
+package com.softwaretesting.testing.customerRegistration.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRegistrationServiceTest {
+
+    @Test
+    void registerNewCustomer() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

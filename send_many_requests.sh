afbbd768fa46efcac6aef6fda587e8e178ace0bd
Valerius Mattfeld <mail@valerius.me>
2024-06-09 12:42:27 +0200

    add dashboard.json for Grafana Dashboard export

--- send_many_requests.sh
+++ send_many_requests.sh
@@ -2,4 +2,4 @@
 
 curl localhost:8080/api/v1/customers
 curl localhost:8080/api/lol
-#seq 5000 | xargs -n1 -P15 curl localhost:8080/actuator/
+seq 5000 | xargs -n1 -P15 curl localhost:8080/actuator/
